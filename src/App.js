6a355edf6b60799ea95b594fd53ab064a0fe83b3
Zahra Said <zahra@zahrasaid.org>
2021-03-31 12:32:09 +0200

    create component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Avatar from "./Avatar";
-
+const homerProfile = {
+  image:
+    "https://www.stickees.com/files/cartoon/the-simpsons/2375-hand-sticker.png",
+  firstName: "Homer",
+  lastName: "Simpson",
+};
+const lisaProfile = {
+  image:
+    "https://www.stickees.com/files/cartoon/the-simpsons/2239-bart-simpson-.png",
+  firstName: "Lisa",
+  lastName: "Simpson",
+};
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Avatar />
-        <Avatar />
+        <Avatar {...homerProfile} />
+        <Avatar {...lisaProfile} />
       </header>
     </div>
   );
--- src/Avatar.js
+++ src/Avatar.js
@@ -1,5 +1,14 @@
-const Avatar = () => {
-  return <h1>Hello World</h1>;
+const Avatar = ({ image, firstName, lastName }) => {
+  console.log(image);
+  console.log(firstName);
+  console.log(lastName);
+  return (
+    <div>
+      <h1>{firstName}</h1>
+      <h1>{lastName}</h1>
+      <img src={image} />
+    </div>
+  );
 };
 
 export default Avatar;
