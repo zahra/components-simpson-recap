270c6f1c1be7a3adb9c1b5a0a1fbba1324df4a07
Zahra Said <zahra@zahrasaid.org>
2021-03-31 10:52:42 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Avatar from "./Avatar";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Avatar />
+        <Avatar />
+      </header>
+    </div>
+  );
+}
+
+export default App;
